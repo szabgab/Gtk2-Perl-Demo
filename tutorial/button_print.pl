8769fbd3f5dfe2a4fffca36347fa7695919b7f00
Gabor Szabo <szabgab@gmail.com>
2005-08-11 22:41:27 +0000

    r4619@gabor:  gabor | 2005-08-11 22:38:58 +0300  Use the same function on both ways of exiting:   [x] and Exit

--- tutorial/button_print.pl
+++ tutorial/button_print.pl
@@ -6,23 +6,23 @@ use warnings;
 # the signal handler it is usually better (more readable)
 # to create a real function and when defining the signal handler
 # provide a reference to this subroutin instead of the anonymous sub.
-# In our case \&handle_exit_button is a reference to the handle_exit_button
+# In our case \&handle_exit is a reference to the handle_exit
 # function that in turn is defined later in the file.
 # This function will print a small message on the consol before exiting the application.
 
 use Gtk2 '-init';
 
 my $window = Gtk2::Window->new;
-$window->signal_connect (destroy => sub { Gtk2->main_quit; });
+$window->signal_connect (destroy => \&handle_exit);
 
 my $button = Gtk2::Button->new("Hello world!");
-$button->signal_connect(clicked=> \&handle_exit_button);
+$button->signal_connect(clicked=> \&handle_exit);
 $window->add($button);
 
 $window->show_all();
 Gtk2->main;
 
-sub handle_exit_button {
+sub handle_exit {
 	print "Exiting...\n";
 	Gtk2->main_quit;
 }
