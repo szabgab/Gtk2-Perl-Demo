99089af122832a930c0c7286d49f930083169bb9
Gabor Szabo <szabgab@gmail.com>
2005-08-27 19:27:07 +0000

    r5603@gabor:  gabor | 2005-08-27 20:30:01 +0300  add signals to the buttons and to the main window destroy thing

--- glade/Phonebook.pm
+++ glade/Phonebook.pm
@@ -5,6 +5,10 @@ AUTOLOAD {
 	print $AUTOLOAD ." in Phonebook\n";
 }
 
+sub on_main_window_destroy {
+	exit;
+}
+
 
 
 1;
--- glade/phonebook.pl
+++ glade/phonebook.pl
@@ -1,9 +1,12 @@
+use strict;
+use warnings;
+
 use Gtk2 -init;
 use Gtk2::GladeXML;
-use GP1;
-$gladexml = Gtk2::GladeXML->new("phonebook/phonebook.glade");
+use Phonebook;
+my $gladexml = Gtk2::GladeXML->new("phonebook/phonebook.glade");
 $gladexml->signal_autoconnect_from_package("Phonebook");
-#$quitbtn = $gladexml->get_widget("Quit");
 Gtk2->main;
+
 
 
