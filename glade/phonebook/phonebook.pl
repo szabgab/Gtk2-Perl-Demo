eb7856a71632914185f272dae3b653e1498d9c19
Gabor Szabo <szabgab@gmail.com>
2005-08-27 19:27:26 +0000

    r5604@gabor:  gabor | 2005-08-27 20:34:04 +0300  move the phonebook files in one directory

--- glade/phonebook/phonebook.pl
+++ glade/phonebook/phonebook.pl
@@ -4,7 +4,7 @@ use warnings;
 use Gtk2 -init;
 use Gtk2::GladeXML;
 use Phonebook;
-my $gladexml = Gtk2::GladeXML->new("phonebook/phonebook.glade");
+my $gladexml = Gtk2::GladeXML->new("phonebook.glade");
 $gladexml->signal_autoconnect_from_package("Phonebook");
 Gtk2->main;
 
