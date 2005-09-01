5976ab149a307be61ec848956a5ab2435d023b3f
Gabor Szabo <szabgab@gmail.com>
2005-09-01 13:05:01 +0000

    r5680@gabor:  gabor | 2005-09-01 10:29:59 +0100  move p1 files into the p1 directory

--- glade/p1/p1.pl
+++ glade/p1/p1.pl
@@ -1,6 +1,6 @@
 use Gtk2 -init;
 use Gtk2::GladeXML;
-$gladexml = Gtk2::GladeXML->new("p1/p1.glade");
+$gladexml = Gtk2::GladeXML->new("p1.glade");
 $gladexml->signal_autoconnect_from_package("main");
 $quitbtn = $gladexml->get_widget("Quit");
 Gtk2->main;
--- glade/p1/p2.pl
+++ glade/p1/p2.pl
@@ -1,7 +1,7 @@
 use Gtk2 -init;
 use Gtk2::GladeXML;
 use GP1;
-$gladexml = Gtk2::GladeXML->new("p1/p1.glade");
+$gladexml = Gtk2::GladeXML->new("p1.glade");
 $gladexml->signal_autoconnect_from_package("GP1");
 $quitbtn = $gladexml->get_widget("Quit");
 Gtk2->main;
