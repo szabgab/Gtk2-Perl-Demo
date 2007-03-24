67eac7a9b4b13068bfc4f85fdd217009d05d9318
Gabor Szabo <szabgab@gmail.com>
2007-03-24 08:51:20 +0000

    add gtk2 example of an editor

---
+++ editor/editor.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+our $AUTOLOAD;
+#my $VERSION = 
+
+
+use Gtk2 -init;
+use Gtk2::GladeXML;
+my $gladexml = Gtk2::GladeXML->new("editor.glade");
+$gladexml->signal_autoconnect_from_package("main");
+Gtk2->main;
+
+
+AUTOLOAD {
+	print $AUTOLOAD . "\n";
+}
+
+sub on_main_window_destroy {
+	Gtk2->main_quit;
+}
+
+sub on_button1_clicked {
+	my $text = $gladexml->get_widget('entry1')->get_text;
+	$gladexml->get_widget('label1')->set_text($text)	
+}
+
+
