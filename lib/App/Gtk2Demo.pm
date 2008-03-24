e337f63529acc79510befb6ca3b936c1670b2fa0
Gabor Szabo <szabgab@gmail.com>
2008-03-24 18:04:55 +0000

    move the demo to a module with a simple script to run it

--- lib/App/Gtk2Demo.pm
+++ lib/App/Gtk2Demo.pm
@@ -2,30 +2,37 @@ package App::Gtk2Demo;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.05';
 
-use Gtk2::GladeXML;
+use App::Gtk2Demo::GUI;
 
-my $gladexml;
 sub run {
-    require App::Gtk2Demo::Glade;
-    $gladexml = Gtk2::GladeXML->new_from_buffer(App::Gtk2Demo::Glade::get_xml());
-    $gladexml->signal_autoconnect_from_package("App::Gtk2Demo");
-    Gtk2->main;
-}
-sub on_main_destroy {
-    _close_app();
-}
-sub on_quit_clicked {
-    _close_app();
+    App::Gtk2Demo::GUI::build_gui;
+
 }
 
-AUTOLOAD {
-    our $AUTOLOAD;
-    print $AUTOLOAD . " <-\n";
-}
-sub DESTROY {
-}
+#use Gtk2::GladeXML;
+#
+#my $gladexml;
+#sub run {
+#    require App::Gtk2Demo::Glade;
+#    $gladexml = Gtk2::GladeXML->new_from_buffer(App::Gtk2Demo::Glade::get_xml());
+#    $gladexml->signal_autoconnect_from_package("App::Gtk2Demo");
+#    Gtk2->main;
+#}
+#sub on_main_destroy {
+#    _close_app();
+#}
+#sub on_quit_clicked {
+#    _close_app();
+#}
+#
+#AUTOLOAD {
+#    our $AUTOLOAD;
+#    print $AUTOLOAD . " <-\n";
+#}
+#sub DESTROY {
+#}
 
 
 1;
